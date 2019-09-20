37ad1447bc3635f6db537415ab5ec4db426b9b03
thefreemavens <thefreemavens@gmail.com>
2019-09-20 13:03:27 +0300

    Deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'The Free Mavens',
+    title: 'The Freemavens',
     meta: [
       { charset: 'utf-8' },
       { name: 'description', content: 'Be a Maven @TheFreemavens. #freemavenry' },
